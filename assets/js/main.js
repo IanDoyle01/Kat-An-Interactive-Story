f1d91cf67f3265dc879bdab62bbee25bcd3c51f6
Ian Doyle <iandoyle01@gmail.com>
2020-02-18 19:16:08 +0000

    adds blank state to store actions as players progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,7 @@
+// Set const variables for targets in index.html
 const sceneGraphic = document.getElementById('graphic')
 const storyText = document.getElementById('story-text')
 const optionElement = document.getElementById('option-buttons')
+
+// Create a blank state to store actions
+state = {}
