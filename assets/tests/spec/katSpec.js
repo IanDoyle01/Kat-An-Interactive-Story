53d6ad8705c77ee76a5be934b179707d11a4658b
Ian Doyle <iandoyle01@gmail.com>
2020-02-25 18:47:26 +0000

    defines test to see if startGame function exists

---
+++ assets/tests/spec/katSpec.js
@@ -0,0 +1,10 @@
+// Test Kat Game
+describe("Kat Game", function() {
+    
+    // Check startGame function
+    describe("will start", function() {
+        it("should exist", function() {
+            expect(startGame).toBeDefined();
+        });    
+    });
+});
