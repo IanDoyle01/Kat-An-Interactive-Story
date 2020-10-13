9b70482dced70619132507dcd44d47bbda40fd9f
Ian Doyle <iandoyle01@gmail.com>
2020-10-13 19:29:45 +0100

    adds test for showOption is defined

--- assets/tests/spec/katSpec.js
+++ assets/tests/spec/katSpec.js
@@ -1,14 +1,19 @@
 // Test Kat Game
 describe("Kat Game", function() {
     
-    // Check startGame function
+    // Check functions are defined
     describe("functions are defined", function() {
-        //Check it exists
+        // Check startGame is defined
         it("start game should exist", function() {
             expect(startGame).toBeDefined();
-        });    
+        });
+        // Check showTextBlock is defined   
         it("show text block should exist", function() {
             expect(showTextBlock).toBeDefined();
+        });
+        // Check showOption is defined
+        it("show option should exist", function() {
+            expect(showOption).toBeDefined();
         });
     });
 });
