cadecc6dc2b9dfbcb02909db746a93c6cf919d07
Ian Doyle <iandoyle01@gmail.com>
2020-10-13 19:24:05 +0100

    adds test for showTextBlock is defined

--- assets/tests/spec/katSpec.js
+++ assets/tests/spec/katSpec.js
@@ -2,10 +2,14 @@
 describe("Kat Game", function() {
     
     // Check startGame function
-    describe("will start", function() {
+    describe("functions are defined", function() {
         //Check it exists
-        it("should exist", function() {
+        it("start game should exist", function() {
             expect(startGame).toBeDefined();
         });    
+        it("show text block should exist", function() {
+            expect(showTextBlock).toBeDefined();
+        });
     });
 });
+
