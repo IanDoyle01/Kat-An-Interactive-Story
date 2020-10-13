72e329780ca070997fef7932cdbe10bf17b4ca9b
Ian Doyle <iandoyle01@gmail.com>
2020-10-13 18:57:22 +0100

    adds comment

--- assets/tests/spec/katSpec.js
+++ assets/tests/spec/katSpec.js
@@ -3,6 +3,7 @@ describe("Kat Game", function() {
     
     // Check startGame function
     describe("will start", function() {
+        //Check it exists
         it("should exist", function() {
             expect(startGame).toBeDefined();
         });    
